d4020e8e5020a017328f1a35206b5424843b90a2
Glitch (asparism-url-shortener-microservice) <none>
2017-10-24 08:40:56 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -32,28 +32,48 @@ var url = process.env.URL;
 var x;
 
 
-// middleware ??
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
+var user = {
+  "user" : process.env.M_LAB_USERNAME,
+  "pwd" : process.env.M_LAB_PASSWORD,
+  "roles" : [
+      {
+          "role" : "userAdminAnyDatabase",
+          "db" : "url-shortener"
+      }
+  ]
+};
+
+
 MongoClient.connect(url, function(err, db){
-  if (err) {console.log('unable to. error is '+err)}
+  if (err) {console.log('unable to connect. error is '+err)}
   else {
     console.log('connected to '+url);
+    db.createUser(user);
+    
     var collection = db.collection('url-shortener');
-    x = collection;
+
     var doc1 = {'hello':'doc1'};
     var doc2 = {'hello':'doc2'};
     var lotsOfDocs =[{'hello': 'doc3'}, {'hello':'doc4'}];
     collection.insert(doc1);
-    collection.insert(doc2, {'w':1}, function(err, result){});
+    collection.insert(doc2, {'w':1}, function(err, result){
+      console.log(err);
+     // console.log("Record added as "+result[0]._id);
+    });
     collection.insert(lotsOfDocs, {w:1}, function(err, result){});
     console.log('finshed insert');
     x = doc2;
+    collection.find({}).toArray(function(err, result) {
+      if (err) {console.log(err)}
+      else {
+        console.log(result.length);
+      }
+    })
+
     }
-    
-     
-     
-    
     db.close();
   }
 );
