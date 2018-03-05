c3cc5d0585976d4f9d66a81071ff892ea0e9ceba
Glitch (asparism-url-shortener-microservice) <none>
2018-03-05 19:36:30 +0000

    🐏👲 Checkpoint server.js:495043/203

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var paramURL, db;
 var uri = process.env.URL;
 
 app.get('/', function(req, res) {
-  res.send('<h2>welcome home</h2><br/>Get a shortened url by pointing your browser in this format:<br/>asparism-url-shortener-microservice.glitch.me<h4>/new/https');
+  res.send('<h2>welcome home</h2><br/>Get a shortened url by pointing your browser in this format:<br/>asparism-url-shortener-microservice.glitch.me/new/https://google.com');
 });
 
 app.get('/new/:incorrect', function(req, res) {
-  res.send({error: 'Incorrect input. Make sure to follow format: https://website.com'});
+  res.send({error: 'Incorrect input. Make sure to follow format: /new/https://website.com'});
 });
 
 app.get('/new/https://:url', function(req, res) {
@@ -30,13 +30,11 @@ app.get('/new/https://:url', function(req, res) {
   }
   var shortURLObj = {
     input : paramURL,
-    short : getShortRandom()
+    short : 'asparism-url-shortener-microservice.glitch.me/'+getShortRandom()
   };
 
   urls.insert(shortURLObj, function(err, results) {
     if (err) throw err;
-    // DROP ALL TEST DATA WHEN YOU'RE FINISHED 
-    // urls.drop something somethign
     console.log('inserted '+shortURLObj.short);
   });
   
@@ -46,7 +44,7 @@ app.get('/new/https://:url', function(req, res) {
 app.get('/:short', function(req, res, next) {
   var urls = db.collection('urls');
   console.log('searched for '+req.params.short);
-  urls.find({ short : req.params.short }, { input : 1 }).toArray(function (err, result) {
+  urls.find({ short : 'asparism-url-shortener-microservice.glitch.me/'+req.params.short }, { input : 1 }).toArray(function (err, result) {
     if (err) throw err;
     console.log('found '+result[0].input);
     var originalURL = result[0].input;
@@ -62,15 +60,6 @@ app.get('/:short', function(req, res, next) {
 
 app.use(express.static("public"));
 
-
-// create a database connection variable outside of the database connection callback
-// to reuse the connection pool in the app
-// var db;
-
-
-// MATH.RANDOM TO GET SHORTENED URL
-// {INPUT : paramUrl, SHORT : RANDOM 4 DIGIT}
-// INSERT IT
 
 // connect to the db before starting the app server
 mongodb.MongoClient.connect(uri, function(err, database) {
